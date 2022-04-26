cf9ec6c930a2a1e972c30417e0bf2d53eb65adba
karlobozic <karlo.bozic@hotmail.com>
2022-04-26 15:50:35 +0200

    changed stars to full only

--- src/components/rating/index.js
+++ src/components/rating/index.js
@@ -17,7 +17,7 @@ function RatingStar() {
         count={5}
         onChange={ratingChanged}
         size={35}
-        isHalf={true}
+        isHalf={false}
         emptyIcon={<i className="far fa-star"></i>}
         halfIcon={<i className="fa fa-star-half-alt"></i>}
         fullIcon={<i className="fa fa-star"></i>}
